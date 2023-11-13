38921e702cfa7126aaf476c3eacb091dd86cc7b2
Taromyk <taromyk@gmail.com>
2023-11-13 16:31:08 +0900

    センサー付き

--- main.ts
+++ main.ts
@@ -1,16 +1,15 @@
-input.onButtonPressed(Button.A, function () {
-    角度 = 45
-    pins.servoWritePin(AnalogPin.P2, 角度)
-    for (let index = 0; index < 4; index++) {
-        角度 += 90
-        pins.servoWritePin(AnalogPin.P2, 角度)
-        basic.pause(500)
+let 角度 = 90
+pins.servoWritePin(AnalogPin.P2, 角度)
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
         角度 += -90
         pins.servoWritePin(AnalogPin.P2, 角度)
         basic.pause(500)
+        角度 += 90
+        pins.servoWritePin(AnalogPin.P2, 90)
+        basic.pause(500)
+    } else {
+        角度 = 90
+        pins.servoWritePin(AnalogPin.P2, 角度)
     }
-    pins.servoWritePin(AnalogPin.P2, 105)
 })
-let 角度 = 0
-角度 = 105
-pins.servoWritePin(AnalogPin.P2, 角度)
