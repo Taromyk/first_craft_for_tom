fd866aa91466e4d6a7e3eb6339d3f79db731051f
Taromyk <taromyk@gmail.com>
2023-11-13 14:12:54 +0900

    デフォルトのサーボブロックを使用

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     角度 = 45
-    servos.P1.setAngle(角度)
+    pins.servoWritePin(AnalogPin.P2, 角度)
     for (let index = 0; index < 4; index++) {
         角度 += 90
-        servos.P1.setAngle(角度)
+        pins.servoWritePin(AnalogPin.P2, 角度)
         basic.pause(500)
         角度 += -90
-        servos.P1.setAngle(角度)
+        pins.servoWritePin(AnalogPin.P2, 角度)
         basic.pause(500)
     }
-    servos.P1.setAngle(105)
+    pins.servoWritePin(AnalogPin.P2, 105)
 })
 let 角度 = 0
 角度 = 105
-servos.P1.setAngle(角度)
+pins.servoWritePin(AnalogPin.P2, 角度)
