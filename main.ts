32d716433ceabf9806a596bb20c01c5c415c70e3
Taromyk <taromyk@gmail.com>
2023-11-13 16:36:45 +0900

    繰り返し

--- main.ts
+++ main.ts
@@ -2,12 +2,14 @@ let 角度 = 90
 pins.servoWritePin(AnalogPin.P2, 角度)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 1) {
-        角度 += -90
-        pins.servoWritePin(AnalogPin.P2, 角度)
-        basic.pause(500)
-        角度 += 90
-        pins.servoWritePin(AnalogPin.P2, 90)
-        basic.pause(500)
+        for (let index = 0; index < 6; index++) {
+            角度 += -90
+            pins.servoWritePin(AnalogPin.P2, 角度)
+            basic.pause(500)
+            角度 += 90
+            pins.servoWritePin(AnalogPin.P2, 90)
+            basic.pause(500)
+        }
     } else {
         角度 = 90
         pins.servoWritePin(AnalogPin.P2, 角度)
