eb35ead5ede967440eb8357e489f67bb01e70e8e
Taromyk <taromyk@gmail.com>
2023-11-18 13:14:15 +0900

    水平から始まる

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
-let 角度 = 90
+let 角度 = 15
 pins.servoWritePin(AnalogPin.P2, 角度)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 1) {
-        for (let index = 0; index < 6; index++) {
+        for (let index = 0; index < 4; index++) {
+            角度 += 90
+            pins.servoWritePin(AnalogPin.P2, 角度)
+            basic.pause(500)
             角度 += -90
             pins.servoWritePin(AnalogPin.P2, 角度)
             basic.pause(500)
-            角度 += 90
-            pins.servoWritePin(AnalogPin.P2, 90)
-            basic.pause(500)
         }
     } else {
-        角度 = 90
+        角度 = 15
         pins.servoWritePin(AnalogPin.P2, 角度)
     }
 })
