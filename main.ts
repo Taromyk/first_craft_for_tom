67c8637eeacbc268fc96482e99e36c20278cfc56
Taromyk <taromyk@gmail.com>
2023-11-18 13:21:47 +0900

    初期値角度60度

--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
-let 角度 = 15
+let 角度 = 20
 pins.servoWritePin(AnalogPin.P2, 角度)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 1) {
         for (let index = 0; index < 4; index++) {
-            角度 += 90
+            角度 = 20
+            角度 += 80
             pins.servoWritePin(AnalogPin.P2, 角度)
             basic.pause(500)
-            角度 += -90
+            角度 += -80
             pins.servoWritePin(AnalogPin.P2, 角度)
             basic.pause(500)
         }
     } else {
-        角度 = 15
+        角度 = 60
         pins.servoWritePin(AnalogPin.P2, 角度)
     }
 })
